b1b2ad206b889eb2910a055bf89b1576450a1220
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-03-27 17:29:19 -0700

    Messing around with new F# for C# programmers book examples

---
+++ Exercises/Exercises/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Exercises.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("Exercises")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("azadi")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Exercises/Exercises/Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main argv = 
+    let mutable sum = 0
+    for i in [0..100] do
+        if i % 2 <> 0 then sum <- sum + i
+    printf "the sum of odd numbers from 0 to 100 is %d \n" sum
+    0
